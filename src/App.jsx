063117a919615aeb0c18a8261056cc6de979dc48
moustafa-hendawy <mostafahendawy26207@gmail.com>
2025-04-27 23:59:50 +0300

    main page with 2 buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
 import React from "react";
 import Employees from './components/Employees';
-import { Route, Routes } from "react-router-dom";
+import { Link, Route, Routes } from "react-router-dom";
 import Faculty from "./components/faculty/Faculty";
 import Update from "./components/faculty/Update";
+import MainPage from "./components/MainPage";
 
 function App() {
   
   return (
-  <Routes>
+<div>
+
+<Routes>
+    <Route path="/" element={<MainPage />} />
     <Route path="/employee" element={<Employees />}/>
     <Route path="/faculty" element={<Faculty />}/>
     <Route path="/faculty/update/:id" element={<Update />}/>
   </Routes>
+</div>
     
 
     
