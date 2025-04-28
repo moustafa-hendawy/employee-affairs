fa76a342cb8fd92239e530120effaf975eeb3efa
moustafa-hendawy <mostafahendawy26207@gmail.com>
2025-04-28 14:46:15 +0300

    change of url faculty

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -8,7 +8,7 @@ function MainPage() {
     <h1 className="d-flex align-items-center justify-content-center">الصفحة الرئيسية</h1>
     <div className="d-flex align-items-center justify-content-center">
     <Link to='/employee' type="button" className="btn btn-primary">To Empolyee Page</Link>
-    <Link to='/faculty' type="button" className="btn btn-secondary">To Faculty Page</Link>
+    <Link to='/faculty' type="button" className="btn btn-danger">To Faculty Page</Link>
     </div>
    </>
   )
